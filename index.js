ec97dc2520003bb36fd87346ae43080fd52460d8
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-02-28 19:14:02 -0500

    purple border

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let myHeader = document.querySelector('#page-header')
+myHeader.style.backgroundColor = 'purple'
+let images = document.querySelectorAll('img')
+for(let x= 0; x< images.length; x++) {
+    images[x].style.border = '5px solid purple'
+    images[x].style.borderRadius = '20px'
+}
 
 // Select some elements...
